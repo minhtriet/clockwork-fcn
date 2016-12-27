2585f965f992a571a88227318b80930949c76c9f
minhtriet <minhtriet09@gmail.com>
2016-12-27 21:13:43 +0100

    Add random, edit gitignore

--- nets/segment.py
+++ nets/segment.py
@@ -1,12 +1,12 @@
-import numpy as np  
+import numpy as np
 import random
 import caffe
 
-from lib import run_net
+from ... import lib.run_net
 
 import yaml
 
-from datasets.kitty import kitty
+from kitty import kitty
 
 random.seed(0xCAFFE)
 
@@ -19,7 +19,7 @@ def keep_layer(layer):
     is_interp = 'up' in layer
     is_type = layer.startswith('score')
     return is_type and not (is_split or is_crop or is_interp)
-    
+
 def segsave(net, path):
     """
     Save maps to disk as compressed arrays.
@@ -29,13 +29,13 @@ def segsave(net, path):
             np.savez('{}-{}.npz'.format(path, layer), blob.data[0])
 
 #caffe.set_device(0)
-#caffe.set_mode_cpu()
+caffe.set_mode_cpu()
 
 
 with open("config.yml", 'r') as ymlfile:
     path = yaml.load(ymlfile)['path']
 
-CS = kitty()
+CS = kitti()
 n_cl = len(CS.classes)
 split = 'training'
 
@@ -60,7 +60,7 @@ for l in layers:
 for ix, frame_name in enumerate(label_frames):
     scene = frame_name.split('_', 1)[0]
     index = frame_name.split('_', 1)[1]
-    im = CS.load_image(split, scene, index)    
+    im = CS.load_image(split, scene, index)
     # handle first frame
     if (ix == 0):
         label = CS.load_label(split, scene, index)
@@ -73,7 +73,7 @@ for ix, frame_name in enumerate(label_frames):
         new_label = CS.load_label(split, scene, index)
         new_data = run_net.segrun(net, CS.preprocess(im))
         new_feats = [net.blobs[l].data[0].copy() for l in layers]
-        new_argmaxes = [net.blobs[l].data[0].argmax(axis=0).copy() for l in layers if 'score' in l]    
+        new_argmaxes = [net.blobs[l].data[0].argmax(axis=0).copy() for l in layers if 'score' in l]
         # compute differences
         for lx, l in enumerate(layers):
             abs_diff = np.abs(new_feats[lx] - feats[lx])
@@ -83,13 +83,13 @@ for ix, frame_name in enumerate(label_frames):
             diffs[l][ix] = rel_diff.mean()
             diffs[l + '-argmax'][ix] = np.array(new_argmaxes[lx] != argmaxes[lx]).mean()
         # TODO: hacking
-        new_data[new_data == 0] = -1    
+        new_data[new_data == 0] = -1
         diffs['data'][ix] = (np.abs(new_data - data) / ((np.abs(new_data) + np.abs(data)) / 2.)).mean()
         print np.unique(data)
-        print np.unique(new_data)        
-        
+        print np.unique(new_data)
+
         diffs['label'][ix] = np.array(new_label != label).mean()
         # advance over old
-        feats, argmaxes, data, label = new_feats, new_argmaxes, new_data, new_label    
-        
+        feats, argmaxes, data, label = new_feats, new_argmaxes, new_data, new_label
+
     np.savez('{}/data_road/{}/image_2/{}/{}'.format(CS.dir, split, scene, ix), **diffs)
